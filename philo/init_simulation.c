4403d566cf78a311f2fb33e97dcef4c2fcebb8fc
hachi868 <dev@hachi868.com>
2023-05-03 22:32:41 +0900

    :dizzy:[update]init philo and take folk

--- philo/init_simulation.c
+++ philo/init_simulation.c
@@ -6,7 +6,7 @@
 /*   By: hachi-gbg <dev@hachi868.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/23 23:27:40 by hachi-gbg         #+#    #+#             */
-/*   Updated: 2023/05/03 18:53:05 by hachi-gbg        ###   ########.fr       */
+/*   Updated: 2023/05/03 19:24:22 by hachi-gbg        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,17 +26,17 @@
 
 // pthread_createの第3引数として渡す。
 // voidポインタをreturnし、voidポインタの引数を1つ取る関数でないといけない。
-// 引数はpthread_create第4引数から受け取れるが、今回は使わないのでNULLとなっている
 void	*thread_func(void *arg)
 {
-	intptr_t	n;
-	//const char	*func_name;
+	t_philo_info	*philo_info;
 
-	n = (intptr_t)arg;
-	//func_name = __func__;
-	//repeat_print(n, func_name);
-	printf("thread_func: philo %ld\n", n);
-	return ((void *)n);
+	philo_info = (t_philo_info *)arg;
+	pthread_mutex_lock(philo_info->spork);
+	pthread_mutex_lock(philo_info->folk);
+	printf("%zu: eating\n", philo_info->index);
+	pthread_mutex_unlock(philo_info->folk);
+	pthread_mutex_unlock(philo_info->spork);
+	return ((void *)philo_info);
 }
 
 void	init_philo_info(t_simulation *philosophers, int i)
@@ -47,14 +47,6 @@ void	init_philo_info(t_simulation *philosophers, int i)
 	num_philo = philosophers->number_of_philosophers;
 	philo_info = philosophers->philo_list[i];
 	philo_info->index = i + 1;
-	philo_info->thread = (pthread_t *)malloc(sizeof(pthread_t));
-	if (pthread_create(\
-		philo_info->thread, NULL, thread_func, (void *)philo_info->index) != 0)
-	{
-		printf("Error!スレッド作れなかった");
-		//todo:free
-		return ;//error
-	}
 	if (i % 2 == 0)
 	{
 		if (i == num_philo - 1)
@@ -73,18 +65,25 @@ void	init_philo_info(t_simulation *philosophers, int i)
 	}
 	philo_info->time_last_eaten = 0;
 	philo_info->count_eaten = 0;
+	philo_info->thread = (pthread_t *)malloc(sizeof(pthread_t));
+	if (pthread_create(\
+		philo_info->thread, NULL, thread_func, (void *)philo_info) != 0)
+	{
+		printf("Error!スレッド作れなかった");
+		//todo:free
+		return ;//error
+	}
 }
 
 int	start_simulation(t_simulation *philosophers)
 {
-	size_t	num_threads;
-	size_t	i;
+	int	num_threads;
+	int	i;
 
 	num_threads = philosophers->number_of_philosophers;
 	i = 0;
 	while (i < num_threads)
 	{
-		printf("start_simulation 1 - %zu\n", i);
 		if (pthread_mutex_init(philosophers->folk_list[i], NULL) != 0)
 			exit(1);//todo:free
 		i++;
@@ -92,7 +91,6 @@ int	start_simulation(t_simulation *philosophers)
 	i = 0;
 	while (i < num_threads)
 	{
-		printf("start_simulation 2 - %zu\n", i);
 		init_philo_info(philosophers, i);
 		i++;
 	}
