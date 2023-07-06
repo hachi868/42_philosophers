c36ad0c666dc311125c3bde7759064314debabf3
hachi868 <24986912+hachi868@users.noreply.github.com>
2023-07-06 17:34:50 +0900

    :robot:[fix]mutex_unlock

--- philo/utils/ft_mutex.c
+++ philo/utils/ft_mutex.c
@@ -6,7 +6,7 @@
 /*   By: hachi-gbg <dev@hachi868.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 14:12:58 by hachi-gbg         #+#    #+#             */
-/*   Updated: 2023/07/06 16:37:55 by hachi-gbg        ###   ########.fr       */
+/*   Updated: 2023/07/06 17:34:21 by hachi-gbg        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,15 @@ int	unlock_mutex_all(t_simulation *ctx_simulation)
 	i = 0;
 	while (i < ctx_simulation->number_of_philosophers)
 	{
-		// pthread_mutex_unlock(ctx_simulation->fork_list[i]);//常にunlockかも。
+		pthread_mutex_unlock(ctx_simulation->fork_list[i]);
 		pthread_mutex_destroy(ctx_simulation->fork_list[i]);
 		i++;
 	}
-	//pthread_mutex_unlock(ctx_simulation->mutex_is_end);//常にunlockかも。
+	pthread_mutex_unlock(ctx_simulation->mutex_is_end);
 	pthread_mutex_destroy(ctx_simulation->mutex_is_end);
 	if (ctx_simulation->number_of_times_each_philosopher_must_eat > 0)
 	{
-		//pthread_mutex_unlock(ctx_simulation->mutex_fill_eat);//常にunlockかも。
+		pthread_mutex_unlock(ctx_simulation->mutex_fill_eat);
 		pthread_mutex_destroy(ctx_simulation->mutex_fill_eat);
 	}
 	return (0);
